d9705879eea0f8e520556c4a56079c55c00f6251
Eder Lima <devilsouls666@gmail.com>
2023-12-12 18:47:29 -0300

    feat: initial project setup

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	go build -o bin/server cmd/server/main.go
+
+run:
+	go run cmd/server/main.go
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmsgprefix)
+	log.SetPrefix(fmt.Sprintf("[%d] [api-gateway] ", os.Getpid()))
+}
+
+func main() {
+	log.Println("Hello, world!")
 }
